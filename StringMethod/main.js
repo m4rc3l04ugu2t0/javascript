6becdc3d9a87027de8a076fb97646500c0c9771f
Marcelo Augusto <mrcelo014@gmail.com>
2024-03-14 14:24:51 -0300

    strings methods

---
+++ StringMethod/main.js
@@ -0,0 +1,22 @@
+let userName = 'Next Level Code'
+let log
+
+// log = userName.charAt(0)
+// log = userName.indexOf('l')
+// log = userName.lastIndexOf('l')
+// log = userName.length
+// log = userName.trim()
+// log = userName.toLowerCase()
+// log = userName.toUpperCase()
+// log = userName.repeat(5)
+// log = userName.startsWith('Next')
+// log = userName.endsWith('de')
+// log = userName.includes('Level')
+
+let numberPhone = '55 86 99999-9999'
+
+// log = numberPhone.replaceAll(' ', '').replace('-', '')
+// log = numberPhone.padStart(20, '5')
+// log = numberPhone.padEnd(20, '0')
+
+console.log(log)

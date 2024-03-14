8f8962b29ea7ff70b454f2cbf1b59e317f1c06f9
Marcelo Augusto <mrcelo014@gmail.com>
2024-03-14 15:08:38 -0300

    ETHOD CHAINING

--- StringMethod/main.js
+++ StringMethod/main.js
@@ -19,4 +19,15 @@ let numberPhone = '55 86 99999-9999'
 // log = numberPhone.padStart(20, '5')
 // log = numberPhone.padEnd(20, '0')
 
+// const firstName = userName.slice(0, userName.indexOf(' '))
+// log = firstName
+
+// const larstName = userName.slice(userName.indexOf(' ') + 1)
+// log = larstName
+
+// ---------- METHOD CHAINING -------------
+userName = 'marceLO'
+userName = userName.trim().charAt(0).toUpperCase() + userName.trim().slice(1).toLowerCase()
+log = userName
+
 console.log(log)
